2ed6b969fb0b3a475df13979eb17abcccee27d2d
Arbaaz <arbaazahussain786@gmail.com>
2024-03-01 14:46:38 +0000

    Created Class LiteraturePrize & Laureate

---
+++ Task-1-Literature-Prize-Winners /src/task1/Laureate.java
@@ -0,0 +1,21 @@
+package task1;
+
+public class Laureate {
+
+    private String name;
+    private String birth_death;
+    private String nations;
+    private String languages;
+    private String genres;
+    private String citation;
+
+    public Laureate(String name, String birth_death, String nations, String languages) {
+        this.name = name;
+        this.birth_death = birth_death;
+        this.nations = nations;
+        this.languages = languages;
+        this.genres = genres;
+        this.citation = citation;
+    }
+
+}
---
+++ Task-1-Literature-Prize-Winners /src/task1/LiteraturePrize.java
@@ -0,0 +1,13 @@
+package task1;
+
+public class LiteraturePrize {
+
+   private int year;
+   private String[] winners;
+
+    public LiteraturePrize(int year, String[] winners) {
+        this.year = year;
+        this.winners = winners;
+    }
+
+}
